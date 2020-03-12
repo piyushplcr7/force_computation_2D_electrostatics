f110f6394917b634ca2c1aa99c69d9b121df37af
ppanchal <ppanchal@student.ethz.ch>
2020-03-12 10:26:08 +0100

    switching directories. commit required.

--- examples/quad_approx_circle.cpp
+++ examples/quad_approx_circle.cpp
@@ -6,44 +6,58 @@
 #include <iostream>
 
 int main() {
+  // Preparing the output files
   std::string fname = "quad_approx_circle.txt";
   std::string fname1 = "lowrank_approx_circle.txt";
   std::string fname2 = "eigvecs.txt";
+  std::string fname3 = "velapprox.txt";
   std::ofstream out(fname);
   std::ofstream out1(fname1);
   std::ofstream out2(fname2);
-  double R = 1.5;
+  std::ofstream out3(fname3);
+
   // Definition of the domain
-  parametricbem2d::ParametrizedCircularArc domain(Eigen::Vector2d(0, 0), R, 0,
-                                                  2 * M_PI);
+  static double R = 1.5;
+  static parametricbem2d::ParametrizedCircularArc domain(Eigen::Vector2d(0, 0),
+                                                         R, 0, 2 * M_PI);
+  static double c = 0.01;
+  /*Eigen::MatrixXd cos_list(2,1);
+  Eigen::MatrixXd sin_list(2,1);
+  cos_list << 1.2, 0;
+  sin_list << 0, 1.8;
+  parametricbem2d::ParametrizedFourierSum
+  domain(Eigen::Vector2d(0,0),cos_list,sin_list,0,2*M_PI);*/
 
   unsigned order = 16;
 
   // Defining the BEM space
   parametricbem2d::ContinuousSpace<1> space;
 
-  // Definition of the shape functional
+  // Definitions of the shape functional
+
+  // F in \int_{\Gamma} F.n dS
   auto F = [&](Eigen::Vector2d X) {
     double x = X(0);
     double y = X(1);
-    return Eigen::Vector2d(x * x * x, y * y * y);
-    //return Eigen::Vector2d(x * x * x, y * y);
+    // return Eigen::Vector2d(x * x * x, y * y * y);
+    return Eigen::Vector2d(x * x * x, y * y);
   };
 
+  // f in \int_{\Omega} f dx. f = div F
   class func {
   public:
     double operator()(const Eigen::Vector2d &X) const {
       double x = X(0);
       double y = X(1);
-      return 3 * x * x + 3 * y * y;
-      //return 3 * x * x + 2 * y;
+      // return 3 * x * x + 3 * y * y;
+      return 3 * x * x + 2 * y;
     }
 
     Eigen::Vector2d grad(const Eigen::Vector2d &X) const {
       double x = X(0);
       double y = X(1);
-      return Eigen::Vector2d(6 * x, 6 * y);
-      //return Eigen::Vector2d(3 * x, 2);
+      // return Eigen::Vector2d(6 * x, 6 * y);
+      return Eigen::Vector2d(6 * x, 2);
     }
   };
 
@@ -67,68 +81,95 @@ int main() {
     return val;
   };
 
-  auto vel_field = [&](double x, double y) {
-     return cos(x) * cos(y);
-    //return x * x * x * y * y;
+  class perturbation {
+  public:
+    // Constructor
+    perturbation(std::shared_ptr<parametricbem2d::AbstractParametrizedCurve> pi)
+        : pi_(pi) {}
+
+    // Evaluation
+    Eigen::Vector2d operator()(double t) const {
+      Eigen::Vector2d point = pi_->operator()(t);
+      Eigen::Vector2d tangent = pi_->Derivative(t);
+      Eigen::Vector2d normal;
+      normal << tangent(1), -tangent(0);
+      normal /= normal.norm();
+      return point + c * cos(point(0)) * cos(point(1)) * normal;
+    }
+
+    Eigen::Vector2d Derivative(double t) const {
+      Eigen::Vector2d pt = pi_->operator()(t);
+      double x = pt(0);
+      double y = pt(1);
+      Eigen::Vector2d tangent = pi_->Derivative(t);
+      Eigen::Vector2d normal;
+      normal << tangent(1), -tangent(0);
+      normal /= normal.norm();
+      return tangent +
+             c * Eigen::Vector2d(-sin((1 + t) * M_PI), cos((1 + t) * M_PI)) *
+                 cos(x) * cos(y) +
+             c * normal * M_PI *
+                 (sin(x) * cos(y) * sin(t) * y - cos(x) * sin(y) * x);
+    }
+
+  private:
+    // parametricbem2d::AbstractParametrizedCurve &pi_;
+    std::shared_ptr<parametricbem2d::AbstractParametrizedCurve> pi_;
   };
+
+  auto pshape_functional = [&](const perturbation &pert) {
+    double val = 0;
+
+    auto integrand = [&](double s) {
+      Eigen::Vector2d pt = pert(s);
+      Eigen::Vector2d tangent = pert.Derivative(s);
+      Eigen::Vector2d normal;
+      normal << tangent(1), -tangent(0);
+      normal /= normal.norm();
+      return F(pt).dot(normal) * tangent.norm();
+    };
+    val += parametricbem2d::ComputeIntegral(integrand, -1, 1, order);
+
+    return val;
+  };
+
+  auto vel_field = [&](double x, double y) {
+    return c * cos(x) * cos(y);
+    // return x * x * x * y * y;
+  };
+
+  auto sp = std::make_shared<parametricbem2d::ParametrizedCircularArc>(
+      Eigen::Vector2d(0, 0), R, 0, 2 * M_PI);
+  perturbation pert(sp);
+
+  std::cout << "check: " << pshape_functional(pert) << std::endl;
 
   std::function<double(double, double)> d_cont = vel_field;
 
-  /*unsigned rlevs = 20;
-  Eigen::ArrayXd rs = Eigen::ArrayXd::LinSpaced(rlevs, R - 0.3, R + 0.3);
-  Eigen::ArrayXd deltars = rs + (-R);
-  Eigen::ArrayXd err = Eigen::ArrayXd::Constant(rlevs, 0);
-
-  // Base level shape functional check
-  double base_sf = shape_functional(mesh);
-  double base_sf_ex = 1.5 * M_PI * std::pow(R, 4);
-  std::cout << "shape functional : " << base_sf
-            << " , exact formula: " << base_sf_ex << std::endl;
-  for (unsigned i = 0; i < rlevs; ++i) {
-    // Definition of the displacement vector field
-    auto d = [&](const Eigen::Vector2d &X) {
-      double x = X(0);
-      double y = X(1);
-      return deltars(i);
-    };
-
-    std::function<double(Eigen::Vector2d)> D = d;
-    // Getting the shape gradient and hessian value for the displacement
-    double shape_gradient = shapeGradient(mesh, f, D, order);
-    double shape_hessian = shapeHessian(mesh, f, D, order);
-    double quad_approx = base_sf_ex + shape_gradient + shape_hessian;
-    double sf_ex = 1.5 * M_PI * std::pow(rs(i), 4);
-    err(i) = fabs(sf_ex - quad_approx) / fabs(sf_ex);
-  }
-  Eigen::MatrixXd output(rlevs, 2);
-  output << deltars, err;
-  out.precision(std::numeric_limits<double>::digits10);
-  out << std::setw(5) << "#deltars" << std::setw(15) << "error" << std::endl;
-  out << output << std::endl;*/
-
-  // numpanels 11 gives a nan eigenvector!?
   unsigned start = 4;
   unsigned maxpanels = 50; // start+1;
   double t = 0.5;
-  // double SF0 = 1.5 * M_PI * std::pow(R, 4);
-  // std::cout << "SF0 " << SF0 << std::endl;
+  //double SF0 = 1.5 * M_PI * std::pow(R, 4);
+  //std::cout << "SF0 " << SF0 << std::endl;
+  parametricbem2d::ParametrizedMesh testmesh(domain.split(5));
+   double ex_calc = shape_functional(testmesh);
+   std::cout << "SF0 calc: " << ex_calc << std::endl;
   // double SFt = 1.5 * M_PI * std::pow(R + t, 4);
   // std::cout << "SFt " << SFt << std::endl;
   Eigen::MatrixXd qa = Eigen::VectorXd::Constant(maxpanels, 0);
   Eigen::MatrixXd lra = Eigen::MatrixXd::Constant(maxpanels, maxpanels, 0);
+  Eigen::MatrixXd vfa = Eigen::MatrixXd::Constant(maxpanels, maxpanels, 0);
 
   for (unsigned numpanels = start; numpanels < maxpanels; numpanels += 1) {
     std::cout << "numpanels " << numpanels << std::endl;
     unsigned dim = space.getSpaceDim(numpanels);
     parametricbem2d::ParametrizedMesh mesh(domain.split(numpanels));
 
-    // double ex_calc = shape_functional(mesh);
-    // std::cout << "SF0 calc: " << ex_calc << std::endl;
+
 
     // Velocity field
     // Eigen::VectorXd d = t * Eigen::VectorXd::Constant(dim, 1);
-     Eigen::VectorXd d = space.Interpolate(vel_field, mesh);
-    //Eigen::VectorXd d = Eigen::VectorXd::Random(dim);
+    Eigen::VectorXd d = space.Interpolate(vel_field, mesh);
 
     // Getting the relevant matrices
     auto bundle = getRelevantQuantities(mesh, f, order, space);
@@ -136,10 +177,14 @@ int main() {
     Eigen::MatrixXd A_pr = bundle.first.second;
     Eigen::VectorXd V = bundle.second;
 
+    // Normalizing the velocity field d wrt the H1 norm
+    d /= sqrt(innerPdt(d, d, A_pr));
+
     // Calculating d*, which represents the linear part in the quadratic
     // functional
     Eigen::VectorXd dstar = Mg.lu().solve(V / 2.);
-    // Calculating the quadratic approximation
+
+    // Calculating the QUADRATIC APPROXIMATION
     double quad_approx =
         (d + dstar).dot(Mg * (d + dstar)) - dstar.dot(Mg * dstar); // + SF0;
     qa(numpanels) = quad_approx;
@@ -152,36 +197,94 @@ int main() {
     Eigen::MatrixXd eigvectors = ges.eigenvectors().real(); // eigenvectors
 
     // To check for NaNs in the eigensolution
-    //std::cout << eigvectors.sum() << std::endl;
+    // std::cout << eigvectors.sum() << std::endl;
 
     // Storing the eigenvalues as std vector for sorting
     std::vector<double> temp(eigvals.data(), eigvals.data() + dim);
     // Sorting while storing the indices
     auto idx = sort_indexes(temp);
 
-    for (unsigned K = 1; K < dim; ++K) {
+    for (unsigned K = 1; K <= dim; ++K) {
       std::cout << "K: " << K << std::endl;
 
       Eigen::MatrixXd eigvectors_K(dim, K);
-      eigvectors_K.col(0) = dstar;
-      // Storing the top K eigenvectors
-      for (unsigned i = 1; i < K; ++i) {
-        eigvectors_K.col(i) = eigvectors.col(idx[i-1]);
+
+      // Adding dstar to the approximation space
+      if (true) {
+        eigvectors_K.col(0) = dstar;
+
+        // Storing the top K eigenvectors
+        for (unsigned i = 1; i < K; ++i) {
+          eigvectors_K.col(i) = eigvectors.col(idx[i - 1]);
+        }
+
+        // Check if inclusion of dstar causes linear dependence
+        Eigen::JacobiSVD<Eigen::MatrixXd> svd(
+            eigvectors_K, Eigen::ComputeThinU | Eigen::ComputeThinV);
+
+        // Eigen::JacobiSVD<Eigen::MatrixXd> svd(eigvectors_K);
+
+        auto rank = svd.rank();
+
+        /*if (numpanels == maxpanels-1 && K <7) {
+                  std::cout << eigvectors_K << std::endl;
+                  std::cout << "rank: " << rank << std::endl;
+            }*/
+
+        if (rank < K) {
+          std::cout << "Problem with including dstar, K = " << K
+                    << " rank = " << rank << std::endl;
+          Eigen::MatrixXd newmat(dim, K);
+          // Storing the top K eigenvectors
+          for (unsigned i = 0; i < K; ++i) {
+            eigvectors_K.col(i) = eigvectors.col(idx[i]);
+          }
+        }
       }
+      // Not adding dstar to the approximation space
+      else {
+        // Storing the top K eigenvectors
+        for (unsigned i = 0; i < K; ++i) {
+          eigvectors_K.col(i) = eigvectors.col(idx[i]);
+        }
+      }
+
       // Doing Gram Schmidt orthogonalization on the chosen eigenvectors
       Eigen::MatrixXd ortho = GramSchmidtOrtho(eigvectors_K, A_pr);
 
+      // Low rank projection for d
+      Eigen::VectorXd d_proj = ortho * ortho.transpose() * A_pr * d;
+
+      // Low rank projection for dstar
+      Eigen::VectorXd dstar_proj = ortho * ortho.transpose() * A_pr * dstar;
+
       // Low rank projection for d+d*
-      Eigen::VectorXd dpds_proj =
-          ortho * ortho.transpose() * A_pr * (d + dstar);
-      double low_rank_approx =
-          dpds_proj.dot(Mg * dpds_proj) - dstar.dot(Mg * dstar); // + SF0;
+      Eigen::VectorXd dpds_proj = d_proj + dstar_proj;
+
+      double low_rank_approx;
+      // Calculating the low rank approximation in Low rank structure preserving
+      // way
+      if (true) {
+        low_rank_approx =
+            dpds_proj.dot(Mg * dpds_proj) - dstar.dot(Mg * dstar); // + SF0;
+      }
+      // Not low rank structure preserving way
+      else {
+        low_rank_approx = (d_proj + dstar).dot(Mg * (d_proj + dstar)) -
+                          dstar.dot(Mg * dstar); // + SF0;
+      }
 
       lra(numpanels, K) = low_rank_approx;
 
-      if (numpanels == maxpanels-1 && K==dim-1) {
+      vfa(numpanels, K) = (d + dstar - dpds_proj).norm();
+
+      if (numpanels == maxpanels - 1 && K == dim - 1) {
         out2.precision(std::numeric_limits<double>::digits10);
         out2 << eigvectors_K << std::endl;
+        for (unsigned i = 0; i < dim; ++i) {
+          std::cout << eigvals(idx[i]) << " ,";
+        }
+        std::cout << std::endl;
       }
     } // Loop over K
 
@@ -196,6 +299,8 @@ int main() {
   out << qa << std::endl;
   out1.precision(std::numeric_limits<double>::digits10);
   out1 << lra << std::endl;
+  out3.precision(std::numeric_limits<double>::digits10);
+  out3 << vfa << std::endl;
 
   return 0;
 }
